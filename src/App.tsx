5a22136d9044595d5f7585126304d1bfe7c37834
CodeSandbox <support@codesandbox.io>
2023-08-21 00:42:50 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,80 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import cloud from './assets/cloud.svg';
+
+const randomNumber = () => Math.round(Math.random());
+const randomPosition = () => Math.floor(Math.random() * 4);
+
+const add = (
+  array: Array<number | null>,
+  prevArray?: Array<number | null>
+) => {
+  if (!array.includes(null)) return array;
+  if (prevArray && !prevArray?.includes(null)) return [
+    randomNumber(),
+    randomNumber(),
+    randomNumber(),
+    randomNumber()
+  ]
+
+  const newArray = prevArray ? [...prevArray] : [...array];
+  let position = randomPosition();
+
+  if (prevArray?.length) {
+    prevArray.map((item, index) => {
+      if (item !== null) {
+        newArray.splice(index, 1, randomNumber())
+      }
+    })
+
+    while (prevArray[position] !== null) {
+      position = randomPosition();
+    }
+  }
+
+  newArray.splice(position, 1, randomNumber());
+  return newArray;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  // inspiration: https://br.pinterest.com/pin/401313016797815226/
+  const [counter, setCounter] = useState<number>(0)
+  const [matrix, setMatrix] = useState<Array<Array<number | null>>>([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMatrix(prevMatrix => {
+        const newMatrix = [...prevMatrix];
+        const newArray = Array(4).fill(null);
+
+        if (newMatrix.length < 10) {
+          newMatrix.unshift(newArray);
+        } else {
+          newMatrix.pop()
+          newMatrix.unshift(newArray);
+        }
+
+        newMatrix[counter] = add(newMatrix[0], newMatrix[counter + 1]);
+
+        return newMatrix;
+      })
+
+      setCounter(count => count++)
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='container'>
+      <img src={cloud} alt='Cloud'/>
+      {matrix.map((array, index) => (
+        <ul key={index} className='rain-line'>
+          {array.map((value, innerIndex) => (
+            <li key={innerIndex}>{value === null ? " " : value}</li>
+          ))}
+        </ul>
+      ))}
     </div>
   );
 }
